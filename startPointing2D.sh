0b0b90c86dd391c4459fcc9340d69bd3befa1b4e
EasyLivinGuitar <marcel.gohsen@web.de>
2015-06-08 17:35:55 +0200

    Updated script to run daemon

--- startPointing2D.sh
+++ startPointing2D.sh
@@ -25,11 +25,8 @@ export PYTHONPATH="$LOCAL_AVANGO/lib/python3.4":"$LOCAL_AVANGO/examples":$AVANGO
 export LD_LIBRARY_PATH="$LOCAL_GUACAMOLE/lib":$GUACAMOLE/lib:$LD_LIBRARY_PATH
 
 # run daemon
-<<<<<<< HEAD
-python3.4 ./daemon.py > /dev/null &
-=======
-#python3.4 ./code/daemon.py > /dev/null &
->>>>>>> 5e8c6300e781c7f71489d957371365f492317dfe
+python3.4 ./code/daemon.py > /dev/null &
+
 
 # run program
 if [[ $* == *-d* ]]
