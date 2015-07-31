7f38c197979857d4eb7f9eee690f53bcaf062296
EasyLivinGuitar <marcel.gohsen@web.de>
2015-07-31 15:24:12 +0200

    Fixed order of logging

--- code/logManager.py
+++ code/logManager.py
@@ -2,32 +2,45 @@ import math
 
 class logManager():
 	header_printed=False
+	dictionary = []
 
 	def __init__(self):
-		self.dictionary = {}
+		pass
 
 	def set(self, key, value):
-		self.dictionary[key] = value
+		self.dictionary.append((key, value))
+		# print(len(self.dictionary))
+
+	def findIndexOf(self, key):
+		for item in self.dictionary: 
+			if item[0]==key:
+				return self.dictionary.index(item)
 
 	def writeToFile(self, path):
+
 		logFile = open(path, "a+")
 
-		#update
-		if( self.dictionary["MovementTime"] >0):
-			self.dictionary["throughput"] = self.dictionary["ID_COMBINED"]/self.dictionary["MovementTime"]
+		MT_index = self.findIndexOf("MovementTime")
+		ID_index = self.findIndexOf("ID_COMBINED")
 
-		#print header only once
-		if(self.header_printed==False):
+		if(self.dictionary[MT_index][1]>0):
+			self.dictionary.append(("Throughput", self.dictionary[ID_index][1]/self.dictionary[MT_index][1]))
+		else:
+			self.dictionary.append(("Throughput", "ERROR"))
+
+
+		# print header only once
+		if(self.header_printed == False):
 			header = ""
-			for key in self.dictionary.keys():
-				header = header + str(key) + ","
-			header = header+"\n\n" 
+			for item in self.dictionary:
+				header = header + str(item[0]) + ", "
 			logFile.write(header)
 			self.header_printed = True
 
-		line = ""
-		for value in self.dictionary.values():
-			line = line + str(value) + ","
-		line = line + "\n"
-		logFile.write(line)	
-		logFile.close()
+		data = ""
+		for item in self.dictionary:
+			data = data + str(item[1]) + ", "
+
+		logFile.write(data + "\n")
+		logFile.close
+		self.dictionary=[]
